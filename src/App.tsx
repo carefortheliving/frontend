3b62acbcab48d0d824e7f552564e69b492b3b3e7
Mihir Kumar <mihirpandey.13@gmail.com>
2021-04-21 01:38:33 +0530

    Recoil dependency removed

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import './App.css';
 import RouterOutlet from 'src/components/common/RouterOutlet/View';
 import { BrowserRouter } from 'react-router-dom';
 import ThemeProvider from 'src/components/common/ThemeProvider/View';
-import RecoilProvider from 'src/components/common/RecoilProvider/View';
 
 function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <RecoilProvider>
-          <RouterOutlet />
-        </RecoilProvider>
+        <RouterOutlet />
       </BrowserRouter>
     </ThemeProvider>
   );
