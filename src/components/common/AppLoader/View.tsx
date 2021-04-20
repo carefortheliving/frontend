d9d1f46d770b259378662fd02060ed0ccb59cc64
Pulkit Singh <pulkit.singh@shipsy.co.in>
2021-04-20 22:08:47 +0530

    .

--- src/components/common/AppLoader/View.tsx
+++ src/components/common/AppLoader/View.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { StylesProps } from '../../../types';
-import withStyles from 'react-jss';
 import { CircularProgress, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
