a7b5dc39a26c37a72ecc7db2dce24a95dbaa0dcd
Venkata Sravan Kumar Gollapudi <gollapudivenkatasravankumar@gmail.com>
2021-04-21 21:11:21 +0530

    UI design for the filter, removed redundant states/cities

---
+++ src/Constants/FilterData.jsx
@@ -0,0 +1,28 @@
+import useGeo from "../hooks/useGeo"
+
+
+const CATEGORIES = ["Blood", "Plasma", "Financial Aid" , "Transport"]
+const STATUS = ["Active", "Completed"]
+
+
+export const FilterData = ()=>{
+    const {states } = useGeo();
+    delete states["Pondicherry"]
+    delete states["Delhi"]
+    states["Delhi"] = [{city: "Delhi City", state: "Delhi"}]
+    states["Pondicherry"] = [{city: "Karaikal", state: "Pondicherry"},{city: "Mahe", state: "Pondicherry"},{city: "Yanam", state: "Pondicherry"}]
+    const newstates =  Object.keys(states).sort().reduce(
+        (obj, key) => { 
+          obj[key] = states[key]; 
+          return obj;
+        }, 
+        {}
+      );
+    return ({
+        categories : CATEGORIES,
+        status : STATUS,
+        state : newstates
+    })
+}
+
+export default FilterData;
