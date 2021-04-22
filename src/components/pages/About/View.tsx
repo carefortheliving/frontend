37106c4de544e175ce86d4a809354b7ed86b1465
Alok Raj <alokr417@gmail.com>
2021-04-22 18:42:20 +0530

    about page added

---
+++ src/components/pages/About/View.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { makeStyles } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Navbar from "src/components/common/Navbar/View";
+import Footer from "src/components/common/Footer/View";
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto",
+  },
+}));
+
+interface SayAbout {}
+
+const About: React.FC<SayAbout> = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+
+      <Navbar showBack />
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        Hello About
+        <Box mt={8}>
+          <Footer />
+        </Box>
+      </main>
+    </div>
+  );
+};
+
+export default About;
