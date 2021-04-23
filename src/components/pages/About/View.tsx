b84cddccf4df111ec421d9aa3279c698d9e83b54
Pulkit Singh <pulkit.singh@shipsy.co.in>
2021-04-23 16:02:29 +0530

    about

--- src/components/pages/About/View.tsx
+++ src/components/pages/About/View.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
-import { makeStyles } from "@material-ui/core";
+import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Navbar from "src/components/common/Navbar/View";
 import Footer from "src/components/common/Footer/View";
 import Box from "@material-ui/core/Box";
+import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
+import HearingIcon from '@material-ui/icons/Hearing';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,6 +17,14 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     height: "100vh",
     overflow: "auto",
+    backgroundColor: theme.palette.background.paper,
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  contentGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
   },
 }));
 
@@ -23,6 +33,45 @@ interface SayAbout {}
 const About: React.FC<SayAbout> = () => {
   const classes = useStyles();
 
+  const renderHeader = () => {
+    return (
+      <Container maxWidth="md">
+        <Typography
+          component="h5"
+          variant="h4"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Our Vision <br></br>
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" paragraph>
+          <i>
+            Caring starts with listening <HearingIcon />. <br/>
+            We are committed to listen to the <strong>voice of pain</strong>{/* <RecordVoiceOverIcon /> */}, <br/>
+            and amplify that voice for those, to hear, who can help. <br/>
+            <br/>
+            If you are seeking help, <br/>
+            kindly click on <strong>Create Request</strong> and generate your request. <br/>
+            We will do our best to make it visible to the world. <br/>
+            <br />
+          </i>
+          {/* - Buddha */}
+        </Typography>
+      </Container>
+    );
+  };
+
+  const renderContent = () => {
+    return (
+      <Grid item md={9}>
+        <Container className={classes.contentGrid} maxWidth="lg">
+          {/**Content goes here */}
+        </Container>
+      </Grid>
+    );
+  };
+
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -30,7 +79,12 @@ const About: React.FC<SayAbout> = () => {
       <Navbar showBack />
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
-        Hello About
+        <div className={classes.heroContent}>
+          {renderHeader()}
+        </div>
+        <Grid container>
+          {renderContent()}
+        </Grid>
         <Box mt={8}>
           <Footer />
         </Box>
