892da3817d678013883a7ac2ff09c667ea0e1c9d
Mihir Kumar <mihirpandey.13@gmail.com>
2021-04-27 18:51:06 +0530

    linting rule inverted

--- src/components/pages/AdminPortal/View.tsx
+++ src/components/pages/AdminPortal/View.tsx
@@ -1,20 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, memo, FC } from 'react';
 import {
-  // Button,
   Container,
   Grid,
   makeStyles,
-  // Typography,
 } from '@material-ui/core';
-// import { TextField } from '@material-ui/core';
-// import withAuth from 'src/components/common/withAuth/View';
 import { useHistory } from 'react-router-dom';
 import { getHomeRoute } from 'src/components/common/RouterOutlet/routerUtils';
-// import { Controller, useForm } from 'react-hook-form';
 import useFirestore from 'src/hooks/useFirestore';
-// import { useSnackbar } from 'src/components/common/SnackbarProvider/View';
-// import { RequestType } from 'src/types';
 import {
   useAppContext,
   changeTitle,
@@ -36,14 +29,7 @@ const AdminPortal: FC<AdminPortalProps> = () => {
   const { dispatch } = useAppContext();
   const classes = useStyles();
   const history = useHistory();
-  // const defaultValues = {
-  //   donorEmail: '',
-  //   donorName: '',
-  // } as Pick<RequestType, 'donorEmail' | 'donorName'>;
-  // const { handleSubmit, control, setValue } = useForm({ defaultValues });
-  // const params = useParams();
-  const { updateRequest, getRequests, getUsefulLinks } = useFirestore();
-  // const snackbar = useSnackbar();
+  const { getRequests, getUsefulLinks } = useFirestore();
   const { isAdmin } = useUser();
 
   useEffect(() => {
@@ -62,134 +48,13 @@ const AdminPortal: FC<AdminPortalProps> = () => {
     const requests = await getRequests({});
     const usefulLinks = await getUsefulLinks();
     downloadFile(JSON.stringify({ requests, usefulLinks }, null, 4), 'careforliving.json');
-    // console.log({ requests, usefulLinks });
   };
-
-  // const prefillData = async () => {
-  //   const existingRequest = await getRequest(params?.docId);
-  //   if (typeof existingRequest === 'object') {
-  //     Object.keys(existingRequest).forEach((key) => {
-  //       setValue(key as any, existingRequest?.[key]);
-  //     });
-  //   }
-  // };
-
-  // const onSubmit = async (data) => {
-  //   try {
-  //     await updateRequest(params?.docId, {
-  //       ...data,
-  //       requestStatus: { value: 'closed', label: 'Closed' },
-  //     });
-  //     snackbar.show('success', `Request closed successfully!`);
-  //     history.push(getHomeRoute());
-  //   } catch (e) {
-  //     snackbar.show('error', `Couldn't close request!`);
-  //   }
-  // };
-
-  // const handleCancel = async () => {
-  //   history.push(getHomeRoute());
-  // };
-
-  // const renderEmail = () => {
-  //   return (
-  //     <Controller
-  //       name={'donorEmail'}
-  //       control={control}
-  //       defaultValue=""
-  //       render={({ field }) => (
-  //         <TextField
-  //           {...field}
-  //           placeholder="Donor's email id"
-  //           style={{ width: '100%', height: '100px' }}
-  //         />
-  //       )}
-  //     />
-  //   );
-  // };
-
-  // const renderDonor = () => {
-  //   return (
-  //     <Controller
-  //       name={'donorName'}
-  //       control={control}
-  //       defaultValue=""
-  //       render={({ field }) => (
-  //         <TextField
-  //           {...field}
-  //           style={{ width: '100%' }}
-  //           placeholder="Donors's Name"
-  //         />
-  //       )}
-  //     />
-  //   );
-  // };
-
-  // const renderSubmit = () => {
-  //   return (
-  //     <Button
-  //       variant="contained"
-  //       color="primary"
-  //       onClick={handleSubmit(onSubmit)}
-  //       style={{ marginRight: '10px' }}
-  //     >
-  //       Submit
-  //     </Button>
-  //   );
-  // };
-
-  // const renderCancel = () => {
-  //   return (
-  //     <Button variant="contained" onClick={handleCancel}>
-  //       Cancel
-  //     </Button>
-  //   );
-  // };
 
   return (
     <Container maxWidth="md">
-      {/* <Typography variant="h3" style={{ marginBottom: "50px" }}>
-              Say thanks
-            </Typography> */}
-      {/* <form onSubmit={handleSubmit(onSubmit)}> */}
       <Grid container spacing={1}>
         <GetAppIcon className={classes.downloadButton} onClick={handleDownloadButtonClick}/>
-        {/* <Grid container>
-            <Grid item xs={6}>
-              <Typography variant="h5">Donors&apos;s Name</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              {renderDonor()}
-            </Grid>
-          </Grid>
-
-          <Grid container>
-            <Grid item xs={6}>
-              <Typography variant="h5">Donors&apos;s Email Id</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              {renderEmail()}
-            </Grid>
-          </Grid> */}
-
-        {/* <Grid
-            container
-            xs={12}
-            sm={12}
-            md={12}
-            // justify="flex-end"
-            className={classes.buttons}
-          >
-            <Grid item xs={12} sm={6} md={4} spacing={2}>
-              {renderSubmit()}
-              {renderCancel()}
-            </Grid>
-            {/* <Grid item xs={12} sm={6} md={4} spacing={2}>
-                  {renderResolve()}
-                </Grid> }
-          </Grid> */}
       </Grid>
-      {/* </form> */}
     </Container>
   );
 };
